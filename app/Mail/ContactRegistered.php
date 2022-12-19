179f5086ef06843ad79b48c55975cd0df123df15
modfayz <modfayz@gmail.com>
2022-12-19 17:58:59 +0100

    ajouter email class

---
+++ app/Mail/ContactRegistered.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Contact;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactRegistered extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contact;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Formulaire de contact >'. $this->contact->nomContact,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        
+        return new Content(
+            view: 'mail.contact',
+            with: [
+                'contact' => $this->contact,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
